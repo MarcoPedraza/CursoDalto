60ade047f8a36b0447d948687957b0cf888fe2d7
Marco <marco.pedraza.palacios@gmail.com>
2022-09-11 21:09:18 +0200

    Mas cosas sobre los bucles, hacer los apuntes

--- bucles.js
+++ bucles.js
@@ -1,7 +1,53 @@
-numero = 0 
+ //let numero = 0 
 
-if (numero < 10) {
-     numero++;
+//if (numero < 10) {
+//     numero++;
 
-     document.write(numero)
+ //    document.write(numero)
+//}
+
+//while (numero < 10) {
+
+ //    numero++;
+
+//     document.write(numero + "<br>")
+
+//}
+
+
+
+//Mientras que el nuimero sea menor que diez, sumale un numero.  Si pusisese menor o igual, sumaria un punto más.
+
+
+ /*
+
+ do {
+          document.write (numero + "<br>")
+          numero ++
+ }
+
+ while ( numero <= 6 );
+
+ */
+
+ // do while, lo ejecuta una vez. despues preg7jnta al whuile y si es falsa no lo vuelve a ejecutar
+
+
+
+ 
+
+let numero = 0;
+
+while (numero < 1000){
+     numero ++;
+     document.write(numero);
+     if (numero == 10){
+          break;
+     }
 }
+
+
+  
+
+
+
